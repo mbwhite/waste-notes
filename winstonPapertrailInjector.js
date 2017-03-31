9ee6e1d5d78dc06ccc633fadb5461d07967ba916
Matthew B White <whitemat@uk.ibm.com>
2017-03-31 20:34:50 +0100

    first commit

---
+++ winstonPapertrailInjector.js
@@ -0,0 +1,81 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+'use strict';
+
+const fs = require('fs-extra');
+const winston = require('winston');
+const sprintf = require('sprintf-js').sprintf;
+
+//
+// Requiring `winston-papertrail` will expose
+// `winston.transports.Papertrail`
+//
+require('winston-papertrail').Papertrail;
+
+/** The json structure that has been specified in the configuration
+ * @private
+ * @param {Object} config JSON structure with specific configuration information
+ * @param {Array} configElements array with the  DEBUG env variables for composer
+ *
+ * @returns {Object} object that is the logger to use
+  */
+exports.getLogger = function (config,configElements){
+
+    let consoleLevel;
+    let logglyLevel;
+
+    if (configElements.debug.length === 0){
+        consoleLevel='error';
+        logglyLevel='info';
+    } else {
+        papertrailLevel=config.papertrail.enabledLevel;
+        consoleLevel=config.console.enabledLevel;
+    }
+
+    let formatterFn = function(options) {
+       // Return string will be passed to logger.
+        return sprintf('%s %-7s %-20s %s'
+       ,options.timestamp()
+       ,options.level.toUpperCase()
+       ,options.message
+       ,(JSON.stringify(options.meta,null,'') +'$')
+      );
+
+    };
+
+    let timestampFn = function() {
+        return new Date(Date.now()).toISOString();
+    };
+
+
+    let newWinstonLogger =  {
+        transports: [
+            new(winston.transports.Papertrail)( {
+                name:'papertrail',
+                host: 'logs5.papertrailapp.com',
+                port: '34662',
+                timestamp: timestampFn,
+                formatter: formatterFn ,
+                level: papertrailLevel,
+                json:true
+            })
+
+        ]
+    };
+
+    winston.loggers.add('Fabric-Composer',newWinstonLogger);
+    return winston.loggers.get('Fabric-Composer');
+
+
+};
